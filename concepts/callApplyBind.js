0d349b2b9839af298f116103ca1e2008f23cabbf
Suman Sourabh <sumsourabh14@gmail.com>
2025-02-14 11:42:47 +0530

    added comments

--- concepts/callApplyBind.js
+++ concepts/callApplyBind.js
@@ -9,8 +9,10 @@ const obj2 = {
   name: "Ronaldo",
 };
 
+// function borrowing
 obj.x.call(obj2, "CR7");
 obj.x.apply(obj2, ["CR7"]);
 
+// make copy of a function
 const y = obj.x.bind(obj2, "CR7");
 y();
